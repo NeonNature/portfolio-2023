7ee8c101e43091dea78b2a3e617b9c525b9dbf45
NeonNature <mustachemaung@gmail.com>
2023-10-18 19:19:11 +0630

    feat: update details page

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,16 +4,12 @@ import '../styles/avatar.css'
 
 export default function Avatar() {
   return (
-    <div className="intro-avatar-wrapper-primary">
-      <div className="intro-avatar-wrapper-secondary">
-        <div className="intro-avatar-container">
-          <Image
-            className="intro-avatar"
-            src={avatar}
-            alt="Profile Picture of Min Maung Maung aka Neon"
-          />
-        </div>
-      </div>
+    <div className="intro-avatar-container">
+      <Image
+        className="intro-avatar"
+        src={avatar}
+        alt="Profile Picture of Min Maung Maung aka Neon"
+      />
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-// import Avatar from './components/Avatar'
 import RotatingName from './components/RotatingName'
 import IntroDivider from './components/waves/IntroDivider'
 import WaveTwo from './components/waves/WaveTwo'
 import './styles/styles.css'
 import RotatingAction from './components/RotatingAction'
+import Avatar from './components/Avatar'
 
 export default function Home() {
   return (
@@ -17,11 +17,33 @@ export default function Home() {
       </div>
       <IntroDivider />
       <div className="content-two">
-        <p className="text-center text-3xl">Who am I?</p>
-        <p>
-          I am your regular everyday normal frontend developer.... except Ive
-          got a dream.{' '}
-        </p>
+        <div className="grid grid-cols-2">
+          <div className="flex items-center justify-center p-4">
+            <div className="details-content">
+              <p>
+                Hey, I&apos;m{' '}
+                <span className="details-highlight-blue">Min Maung Maung</span>{' '}
+                aka <span className="details-highlight-green">Neon</span>, your
+                friendly web guru, always chasing{' '}
+                <span className="details-highlight-purple">
+                  pixel perfection
+                </span>
+                , and slightly obsessed with animations. But here&apos;s the
+                kicker – I&apos;ve got a secret love for time travel.
+              </p>
+              <p className="mt-4">
+                While I can&apos;t make you jump through centuries, I can sure
+                take your website on a journey through pixel magic and
+                animations that&apos;ll make it feel like a time warp. Buckle
+                up, because we&apos;re about to make the digital clock spin
+                backward!
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <Avatar />
+          </div>
+        </div>
       </div>
       <WaveTwo />
       <div className="content-three"> aaaaaa </div>
