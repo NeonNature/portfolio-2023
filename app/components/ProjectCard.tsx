6de6e5247528b0a1cc0c92f36a665a8ab4469d37
NeonNature <mustachemaung@gmail.com>
2023-10-28 01:14:44 +0630

    feat: add feature card

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,67 +1,80 @@
 'use client'
 import './../styles/projects.css'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import Image from 'next/image'
 import math from '../../public/math.gif'
+
+interface CardProps {
+  trigger: number
+  active: number
+  setActive: (arg: number) => void
+  className: string
+}
+
+const Card = ({ trigger, active, setActive, className }: CardProps) => {
+  const classHelper = useMemo(() => {
+    if (active === trigger) return 'expand'
+    else return ''
+  }, [trigger, active])
+
+  return (
+    <div
+      className={`project-card ${classHelper} ${className}`}
+      onClick={() => setActive(trigger)}
+    >
+      <div className="project-title">Lan Kyone</div>
+      <div className="project-content">
+        <Image
+          className="action-icon"
+          src={math}
+          alt="Animated icon of math symbols popping out of a book"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default function ProjectCard() {
   const [active, setActive] = useState(0)
 
   return (
     <div className="flex flex-wrap">
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
-
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
-
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
-
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
-
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
-
-      <div className="project-card">
-        <Image
-          className="action-icon"
-          src={math}
-          alt="Animated icon of math symbols popping out of a book"
-        />
-        Lan Kyone
-      </div>
+      <Card
+        trigger={1}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
+      <Card
+        trigger={2}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
+      <Card
+        trigger={3}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
+      <Card
+        trigger={4}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
+      <Card
+        trigger={5}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
+      <Card
+        trigger={6}
+        active={active}
+        setActive={setActive}
+        className="project-bg-1"
+      />
     </div>
   )
 }
