4fc282986bda6c26dcd3cea9bc52c16fe949d5fe
NeonNature <mustachemaung@gmail.com>
2023-11-14 02:03:53 +0630

    feat: add placeholder testimonials

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -15,6 +15,19 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 
+const Testimonial = () => {
+  return (
+    <div>
+      <Image
+        className="testimonial-avatar"
+        src={LanKyone}
+        alt="Animated icon of math symbols popping out of a book"
+      />
+      <div className="testimonial-card">AAAAAA</div>
+    </div>
+  )
+}
+
 export default function Testimonials() {
   const [active, setActive] = useState(0)
 
@@ -34,15 +47,30 @@ export default function Testimonials() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
       </Swiper>
     </div>
   )
