bdaa002d87f5637b0336d2dc6023921560907297
NeonNature <mustachemaung@gmail.com>
2023-11-27 18:02:43 +0630

    fix: add sharp for missing images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'voyager-neon.tech',
+      'www.voyager-neon.tech',
+      'voyager-neon.netlify.app',
+    ],
+  },
+}
 
 module.exports = nextConfig
